a141e1c253aeb20b950c02204df892ee1d5668b1
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2024-02-09 11:39:56 +0300

    add feature user creat new date of birth

--- src/cmd_manager.rs
+++ src/cmd_manager.rs
@@ -2,24 +2,21 @@ use crate::storage::{Person, PersonStorage};
 use crate::ui::UI;
 use chrono::NaiveDate;
 
+use rand::distributions;
+use rand::{thread_rng, Rng};
 use std::{io, vec};
 
 pub struct TUI {}
 
 impl UI for TUI {
-    fn add_info(&self, data: &crate::storage::PersonStorage) -> String {
-        println!("Введите новый id?");
-        //let mut new_id: Vec<i32> = vec![];
-        let mut id: String = String::new();
-        io::stdin().read_line(&mut id).expect("Can't read new id.");
-        let new_id = id.parse::<i32>().unwrap_or_default();
-
-        // for i in id.lines() {
-        //     new_id.push(i.parse::<i32>().unwrap_or_default());
-        // }
-
+    fn add_info(&self, data: &PersonStorage) -> String {
+        //Пользователь получает сгенерированный id.
+        let random_num: i32 = rand::thread_rng().gen_range(1, 10000);
+        let new_id = random_num;
+        
+        //Создаем вектор, в который будут записываться новые данные.
+        //Пользователь вводит новый name и записывает в вектор.
         let mut new_person: Vec<_> = Vec::new();
-
         println!("\n");
         println!("Введите новый name: ");
         let mut new_name = String::new();
@@ -30,6 +27,7 @@ impl UI for TUI {
             new_person.push(line.to_string());
         }
 
+        //Пользователь вводит новый surname и записывает в вектор.
         println!("Введите новый surname: ");
         let mut new_surname = String::new();
         io::stdin()
@@ -39,20 +37,22 @@ impl UI for TUI {
             new_person.push(line.to_string());
         }
 
+        //Пользователь вводит новый surname и записывает в вектор.
         println!("Введите новый middle name: ");
         let mut new_middle_name = String::new();
         io::stdin()
             .read_line(&mut new_middle_name)
-            .expect("Can't read new name.");
+            .expect("Can't read new middle name.");
         for line in new_middle_name.lines() {
             new_person.push(line.to_string());
         }
 
+        //Пользователь вводит новый date of birth.
         println!("Введите новый date of birth в формате 'yyyy-mm-dd': ");
         let mut string_date: String = String::new();
         io::stdin()
             .read_line(&mut string_date)
-            .expect("Wrong date.");
+            .expect("Wrong date format.");
 
         let new_date = &string_date.len();
         string_date.truncate(new_date - 1);
@@ -60,20 +60,27 @@ impl UI for TUI {
 
         let parsed_date = NaiveDate::parse_from_str(&new_date_str, "%Y-%m-%d").unwrap();
 
+        //Пользователь вводит новый gender.
         let mut new_gender_string: String = String::new();
-        println!("Введите gender М/Ж ?:");
+        println!("Введите gender male/female ?:");
         io::stdin()
             .read_line(&mut new_gender_string)
             .expect("Can't read gender");
 
-        let new_gender;
-        if new_gender_string == "М" {
-            new_gender: bool = true;
-        } else if new_date_str == "Ж" {
-            new_gender: bool = false;
+        println!("new_gender_string: {}", new_gender_string);
+
+        let mut new_gender: bool = true;
+        for line in new_gender_string.lines() {
+            if line == "male" {
+                new_gender = true;
+            } else if line == "female" {
+                new_gender = false;
+            } else {
+                println!("Wrong gender format.")
+            }
         }
 
-        let person = Person {
+        let new_person = Person {
             name: new_person[0].to_string(),
             surname: new_person[1].to_string(),
             middle_name: new_person[2].to_string(),
@@ -81,7 +88,7 @@ impl UI for TUI {
             gender: new_gender,
         };
 
-        data.add(new_id, person);
+        data:PersonStorage.add(new_id, new_person);
         return "good".to_string();
     }
 
